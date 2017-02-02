63d12fc9f135506d9909d9b5513afc1a0e13518b
Adam Homer <ahomer@crestfinancial.com>
2017-02-02 14:52:25 -0700

    Added the highlite script- Still in testing.

---
+++ scripts/mods/player/highlitePlayers.lua
@@ -0,0 +1,57 @@
+if onServer() then
+
+package.path = package.path .. ";data/scripts/lib/?.lua"
+package.path = package.path .. ";data/scripts/?.lua",
+require ("randomext")
+
+local consecutiveJumps = 0
+local noSpawnTimer = 0
+
+function initialize()
+	if onClient() then
+        Player():registerCallback("onPreRenderHud", "onPreRenderHud")
+    end
+    if onServer() then
+    	invokeClientFunction(Player().index, "initialize")
+end
+
+function onSectorEntered(player, x, y)
+	var sector = Sector(player)
+end
+
+function onPreRenderHud()
+
+    local ship = Entity()
+    local shipPos = ship.translationf
+
+    local nearby = {Sector():getEntitiesByType(EntityType.Ship)}
+    local displayed = {}
+
+    -- detect all asteroids in range
+    for _, entity in pairs(nearby) do
+
+        if entity:isManned() then
+
+            local d = distance2(entity.translationf, shipPos)
+
+            table.insert(displayed, {ship = entity, distance = d})
+        end
+
+    end
+
+    -- sort by distance
+    table.sort(displayed, sort)
+
+    -- display nearest x
+    local renderer = UIRenderer()
+
+    for i = 1, #displayed do
+        local tuple = displayed[i]
+        -- Render bright green targeter Hex value = #5EF320
+        renderer:renderEntityTargeter(tuple.ship, ColorRGB(94,243,32));
+        -- uncomment if you would like an arrow
+        -- renderer:renderEntityArrow(tuple.asteroid, 30, 10, 250, tuple.material.color, 0);
+    end
+
+    renderer:display()
+end
---
+++ scripts/server.lua
@@ -0,0 +1,59 @@
+package.path = package.path .. ";data/scripts/lib/?.lua"
+package.path = package.path .. ";data/scripts/server/?.lua"
+require ("factions")
+require ("randomext")
+require ("stringutility")
+
+function onStartUp()
+    Server():registerCallback("onPlayerLogIn", "onPlayerLogIn")
+    Server():registerCallback("onPlayerLogOff", "onPlayerLogOff")
+    Galaxy():registerCallback("onPlayerCreated", "onPlayerCreated")
+    Galaxy():registerCallback("onFactionCreated", "onFactionCreated")
+end
+
+function onShutDown()
+
+end
+
+function update(timeStep)
+    local guardianRespawnTime = Server():getValue("guardian_respawn_time")
+    if guardianRespawnTime then
+
+        guardianRespawnTime = guardianRespawnTime - timeStep;
+        if guardianRespawnTime < 0 then
+            guardianRespawnTime = nil
+        end
+
+        Server():setValue("guardian_respawn_time", guardianRespawnTime)
+    end
+
+end
+
+function onPlayerCreated(index)
+    local player = Player(index)
+    Server():broadcastChatMessage("Server", 0, "Player %s created!"%_t, player.name)
+end
+
+function onFactionCreated(index)
+
+end
+
+function onPlayerLogIn(playerIndex)
+    local player = Player(playerIndex)
+    Server():broadcastChatMessage("Server", 0, "Player %s joined the galaxy"%_t, player.name)
+    player:addScriptOnce("headhunter.lua")
+    player:addScriptOnce("eventscheduler.lua")
+    player:addScriptOnce("story/spawnswoks.lua")
+    player:addScriptOnce("story/spawnai.lua")
+    player:addScriptOnce("story/spawnguardian.lua")
+    player:addScriptOnce("story/spawnadventurer.lua")
+    player:addScriptOnce("mods/player/highlitePlayers.lua")
+    matchResources(player)
+end
+
+function onPlayerLogOff(playerIndex)
+    local player = Player(playerIndex)
+    Server():broadcastChatMessage("Server", 0, "Player %s left the galaxy"%_t, player.name)
+
+end
+
