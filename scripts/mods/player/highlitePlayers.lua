73898e38203b42d41c4f788a52489a4db7352727
Adam Homer <ahomer@crestfinancial.com>
2017-02-02 17:48:11 -0700

    Finalized the highliter

--- scripts/mods/player/highlitePlayers.lua
+++ scripts/mods/player/highlitePlayers.lua
@@ -1,58 +1,36 @@
-if onServer() then
-
+package.path = package.path .. ";data/scripts/systems/?.lua"
 package.path = package.path .. ";data/scripts/lib/?.lua"
-package.path = package.path .. ";data/scripts/?.lua",
 require ("randomext")
 
-local consecutiveJumps = 0
-local noSpawnTimer = 0
-
 function initialize()
-	if onClient() then
-        Player():registerCallback("onPreRenderHud", "onPreRenderHud")
-    end
-    if onServer() then
-    	invokeClientFunction(Player(), "initialize")
-    end
-end
-
-function onSectorEntered(player, x, y)
-	var sector = Sector(player)
+    Player():registerCallback("onPreRenderHud", "onPreRenderHud")
 end
 
 function onPreRenderHud()
 
-    local ship = Entity()
-    local shipPos = ship.translationf
-
     local nearby = {Sector():getEntitiesByType(EntityType.Ship)}
     local displayed = {}
-
-    -- detect all asteroids in range
+    print(#nearby)
     for _, entity in pairs(nearby) do
+--        if entity:isManned() then
 
-        if entity:isManned() then
 
-            local d = distance2(entity.translationf, shipPos)
-
-            table.insert(displayed, {ship = entity, distance = d})
-        end
+            table.insert(displayed, {ship = entity})
+--        end
 
     end
-
-    -- sort by distance
-    table.sort(displayed, sort)
-
+    print(#displayed)
     -- display nearest x
     local renderer = UIRenderer()
 
     for i = 1, #displayed do
         local tuple = displayed[i]
-        -- Render bright green targeter Hex value = #5EF320
-        renderer:renderEntityTargeter(tuple.ship, ColorRGB(94,243,32));
-        -- uncomment if you would like an arrow
-        -- renderer:renderEntityArrow(tuple.asteroid, 30, 10, 250, tuple.material.color, 0);
+        -- uncomment to render targeter
+        -- renderer:renderEntityTargeter(tuple.ship, ColorRGB(94,243,32), 0);
+        -- comment if you don't like the an arrow
+        renderer:renderEntityArrow(tuple.ship, 30, 10, 250, ColorRGB(94,243,32), 0);
     end
 
     renderer:display()
 end
+
